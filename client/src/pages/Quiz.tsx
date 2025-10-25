4cffc51f3adefe81f8d0573c644bd345fdc17840
Fay4z <fayazjr212@gmail.com>
2025-10-25 19:26:09 +0300

    add card layout for quiz page

--- client/src/pages/Quiz.tsx
+++ client/src/pages/Quiz.tsx
@@ -1,6 +1,38 @@
+import BasicsCard from "@/components/chess/BasicsCard"
+
 const Quiz = () => {
+    const quizCardData = [
+        {
+            cardHeader: "Beginner Quiz",
+            cardContent: "Learn about chess peices, chess board, and the each peice points",
+            cardLink: "/puzzles",
+            cardLinkText: "Get Started"
+        },
+        {
+            cardHeader: "Intermediate Quiz",
+            cardContent: "Learn about movement of chess peices and chess peice captures",
+            cardLink: "/openings",
+            cardLinkText: "Get Started"
+        },
+        {
+            cardHeader: "Advanced Quiz",
+            cardContent: "Learn about chess rules including En pasent",
+            cardLink: "/openings",
+            cardLinkText: "Get Started"
+        }
+    ]
     return (
-        <div>This is the quiz page</div>
+        <div className="flex flex-row justify-center space-x-10 m-20">
+      {quizCardData.map((data, index) => (
+        <BasicsCard 
+        key={index}
+        cardHeader = {data.cardHeader}
+        cardContent = {data.cardContent}
+        cardLink = {data.cardLink}
+        cardLinkText = {data.cardLinkText}
+        />
+      ))}
+    </div>
     )
 }
 
